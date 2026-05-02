4e47c37da5188f8b902362f7dd29367c3ce8dd59
jiasheng <jiashengguo@outlook.com>
2026-05-02 17:56:40 +0800

    inital commit

---
+++ main.ts
@@ -0,0 +1,29 @@
+import { ZenStackClient } from '@zenstackhq/orm';
+import { SqliteDialect } from '@zenstackhq/orm/dialects/sqlite';
+import SQLite from 'better-sqlite3';
+import { schema } from './zenstack/schema';
+
+async function main() {
+    const db = new ZenStackClient(schema, {
+        dialect: new SqliteDialect({
+            database: new SQLite('./zenstack/dev.db'),
+        }),
+    });
+    const user = await db.user.create({
+        data: {
+            email: 'test@zenstack.dev',
+            posts: {
+                create: [
+                    {
+                        title: 'Hello World',
+                        content: 'This is a test post',
+                    },
+                ],
+            },
+        },
+        include: { posts: true }
+    });
+    console.log('User created:', user);
+}
+
+main();
